e694c53b3bda337dc96c7fa752b3f55989142dcd
Ngoc Son Nguyen <nguyenngocson4391@gmail.com>
2020-07-01 16:55:51 +0700

    test auto create

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import mongoose from 'mongoose'
+import {
+  mongoose
+} from './models/db'
 
 import app from './app'
 import {
--- src/models/project.model.js
+++ src/models/project.model.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose'
+import { mongoose } from './db'
 
 const schema = mongoose.Schema({
   projectName: String,
