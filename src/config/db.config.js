064d086584235f01bedd3ad76477cff03cb9940f
Ngoc Son Nguyen <nguyenngocson4391@gmail.com>
2020-07-02 15:57:04 +0700

    change db url

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,8 +2,7 @@ const DB_USERNAME = process.env.DB_USERNAME || 'admin';
 const DB_PASSWORD = process.env.DB_PASSWORD || 'April654321&';
 const DB_NAME = process.env.DB_NAME || 'feedback';
 
-const DB_URL = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@realmcluster.m9oi2.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-
+const DB_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@realmcluster.m9oi2.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 // const DB_URL = `mongodb://localhost:27001/${DB_NAME}`;
 
 export default DB_URL;
