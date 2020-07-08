5212842a888894e61960f6fb143fe9b5276066eb
Ngoc Son Nguyen <nguyenngocson4391@gmail.com>
2020-07-08 17:32:38 +0700

    response for signin with token

--- src/routes/signin-token.route.js
+++ src/routes/signin-token.route.js
@@ -4,9 +4,12 @@ import HttpStatus from 'http-status-codes';
 const routes = () => {
   const router = express.Router();
   router.post('/', async (req, res) => {
+    const { firstName, lastName, username, roles } = req.user;
     res.status(HttpStatus.OK).json({
-      username: req.user.username,
-      email: req.user.email,
+      username,
+      firstName,
+      lastName,
+      roles,
     });
   });
 
