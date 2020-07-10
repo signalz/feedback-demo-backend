fac439562e3e772ecd57423147c2fb5607a3c538
Ngoc Son Nguyen <nguyenngocson4391@gmail.com>
2020-07-10 14:20:30 +0700

    protected

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import express from 'express';
-import morgan from 'morgan';
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import express from 'express'
+import morgan from 'morgan'
 
 import {
   FeedbackRoutes,
@@ -14,17 +14,17 @@ import {
   SigninToken,
   SurveyRoutes,
   UserRoutes,
-} from './routes';
-import passport, { generateToken } from './middlewares/authentication';
+} from './routes'
+import passport, { generateToken } from './middlewares/authentication'
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
-app.use(morgan('dev'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+app.use(morgan('dev'))
 
-app.use('/signup', SignupRoutes());
+app.use('/signup', passport.authenticate('jwt', { session: false }), SignupRoutes())
 app.use(
   '/signin',
   passport.authenticate('local', {
@@ -32,30 +32,30 @@ app.use(
   }),
   generateToken,
   SigninRoutes(),
-);
+)
 
-app.use('/signin-with-token', passport.authenticate('jwt', { session: false }), SigninToken());
+app.use('/signin-with-token', passport.authenticate('jwt', { session: false }), SigninToken())
 
-app.use('/api/projects', passport.authenticate('jwt', { session: false }), ProjectRoutes());
+app.use('/api/projects', passport.authenticate('jwt', { session: false }), ProjectRoutes())
 
-app.use('/api/sections', passport.authenticate('jwt', { session: false }), SectionRoutes());
+app.use('/api/sections', passport.authenticate('jwt', { session: false }), SectionRoutes())
 
-app.use('/api/surveys', passport.authenticate('jwt', { session: false }), SurveyRoutes());
+app.use('/api/surveys', passport.authenticate('jwt', { session: false }), SurveyRoutes())
 
-app.use('/api/feedbacks', passport.authenticate('jwt', { session: false }), FeedbackRoutes());
+app.use('/api/feedbacks', passport.authenticate('jwt', { session: false }), FeedbackRoutes())
 
 app.use(
   '/api/dashboard/projects/summary',
   passport.authenticate('jwt', { session: false }),
   OverviewDashboardRoutes(),
-);
+)
 
 app.use(
   '/api/dashboard/projects/history',
   passport.authenticate('jwt', { session: false }),
   HistoryDashboardRoutes(),
-);
+)
 
-app.use('/api/users', passport.authenticate('jwt', { session: false }), UserRoutes());
+app.use('/api/users', passport.authenticate('jwt', { session: false }), UserRoutes())
 
-export default app;
+export default app
